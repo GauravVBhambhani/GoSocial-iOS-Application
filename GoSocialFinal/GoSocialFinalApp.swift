1555c73f2f92b90bb3cebb62d85d8008181ad5a2
Gaurav Bhambhani <113461154+GauravVBhambhani@users.noreply.github.com>
2023-04-26 19:39:18 -0400

    Initial Commit

---
+++ GoSocialFinal/GoSocialFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoSocialFinalApp.swift
+//  GoSocialFinal
+//
+//  Created by Gaurav Bhambhani on 4/26/23.
+//
+
+import SwiftUI
+
+@main
+struct GoSocialFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
